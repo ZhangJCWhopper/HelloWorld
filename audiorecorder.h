78582d78d061685b24a2760723e34be9789022c1
ZhangJCWhopper <zhangjiacen@gmail.com>
2017-07-23 15:38:19 +0900

    All in

---
+++ audiorecorder.h
@@ -0,0 +1,15 @@
+#ifndef AUDIORECORDER_H
+#define AUDIORECORDER_H
+
+#include <QMainWindow>
+#include <QMediaRecorder>
+#include <QUrl>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class AudioRecorder;}
+class QAudioRecorder;
+class QAudioProbe;
+class QAudioBuffer;
+
+QT_END_NAMESPACE
+#endif // AUDIORECORDER_H
